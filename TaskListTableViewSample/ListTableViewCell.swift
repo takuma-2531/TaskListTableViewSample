256eb9f5d94e216d799d0b9245bce264da3e0971
Takuma Ogawa <nyabo10_31@yahoo.co.jp>
2021-05-06 14:29:03 +0900

    xibでTableViewCellを作成

---
+++ TaskListTableViewSample/ListTableViewCell.swift
@@ -0,0 +1,31 @@
+//
+//  ListTableViewCell.swift
+//  TaskListTableViewSample
+//
+//  Created by 小川卓馬 on 2021/05/06.
+//
+
+import UIKit
+
+class ListTableViewCell: UITableViewCell {
+    
+    static var identifier: String {
+        String(describing: self)
+    }
+    
+    static var nib: UINib {
+        UINib(nibName: String(describing: self), bundle: nil)
+    }
+
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+    override func setSelected(_ selected: Bool, animated: Bool) {
+        super.setSelected(selected, animated: animated)
+
+        // Configure the view for the selected state
+    }
+    
+}
--- TaskListTableViewSample/ListTableViewController.swift
+++ TaskListTableViewSample/ListTableViewController.swift
@@ -8,12 +8,35 @@
 import UIKit
 
 class ListTableViewController: UIViewController {
-
-  override func viewDidLoad() {
-    super.viewDidLoad()
-    // Do any additional setup after loading the view.
-  }
-
-
+    
+    @IBOutlet weak var tableView: UITableView!
+    
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+        tableView.dataSource = self
+        tableView.delegate = self
+        tableView.register(ListTableViewCell.nib, forCellReuseIdentifier: ListTableViewCell.identifier)
+        
+    }
+    
 }
 
+extension ListTableViewController: UITableViewDataSource {
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return 5
+    }
+    
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        let cell = tableView.dequeueReusableCell(withIdentifier: ListTableViewCell.identifier, for: indexPath) as! ListTableViewCell
+        
+        return cell
+    }
+    
+    
+}
+
+extension ListTableViewController: UITableViewDelegate {
+    
+}
